2c7ecbdb6a9e4c0777eeb18b88f871785423238c
Ítalo Seixas <imsseixas@gmail.com>
2023-07-19 02:12:48 -0300

    <3

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -11,7 +11,7 @@ const Newsletter = () => (
       <p className="p__opensans">E nunca perca as últimas atualizações!</p>
     </div>
     <div className="app__newsletter-input flex__center">
-      <input type="email" placeholder="Enter your email address" />
+      <input type="email" placeholder="Seu melhor e-mail" />
       <button type="button" className="custom__button">Se inscrever</button>
     </div>
   </div>
