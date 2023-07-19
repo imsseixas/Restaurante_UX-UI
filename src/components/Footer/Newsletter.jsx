87fb8a49fd67239438379ac46c0c9dce5293f242
Ítalo Seixas <imsseixas@gmail.com>
2023-07-19 02:08:08 -0300

    S2

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -6,13 +6,13 @@ import './Newsletter.css';
 const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
-      <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
-      <p className="p__opensans">And never miss latest Updates!</p>
+      <SubHeading title="Boletim informativo" />
+      <h1 className="headtext__cormorant">Assine o nosso boletim informativo</h1>
+      <p className="p__opensans">E nunca perca as últimas atualizações!</p>
     </div>
     <div className="app__newsletter-input flex__center">
       <input type="email" placeholder="Enter your email address" />
-      <button type="button" className="custom__button">Subscribe</button>
+      <button type="button" className="custom__button">Se inscrever</button>
     </div>
   </div>
 );
